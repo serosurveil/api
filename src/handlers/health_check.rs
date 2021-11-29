9f67b960e72f76d26c0a8b207c2e601f7b297311
Russ Weas <russweas@gmail.com>
2021-11-29 13:17:54 -0600

    Test

--- src/handlers/health_check.rs
+++ src/handlers/health_check.rs
@@ -6,3 +6,7 @@ use super::ApplicationResponse;
 pub async fn health_check() -> ApplicationResponse {
     Ok(HttpResponse::Ok().finish())
 }
+
+pub async fn say_hello() -> ApplicationResponse {
+    Ok(HttpResponse::Ok().body("Hello, there"))
+}
